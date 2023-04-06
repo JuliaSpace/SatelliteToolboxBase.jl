d3595a866b37f4fbfdad4676add79f2503153cc9
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-04-06 16:39:21 -0300

    :racehorse: Add precompilation for the new functions

--- src/precompile.jl
+++ src/precompile.jl
@@ -22,7 +22,7 @@ SnoopPrecompile.@precompile_all_calls begin
     #                                        Orbit
     ########################################################################################
 
-    KeplerianElements(
+    ke = KeplerianElements(
         date_to_jd(1986, 6, 19, 18, 35, 0),
         7130.982e3,
            0.0001111,
@@ -32,7 +32,7 @@ SnoopPrecompile.@precompile_all_calls begin
          123.456 |> deg2rad,
     )
 
-    KeplerianElements(
+    ke_f32 = KeplerianElements(
         date_to_jd(1986, 6, 19, 18, 35, 0),
         7130.982f3,
            0.0001111f0,
@@ -42,17 +42,42 @@ SnoopPrecompile.@precompile_all_calls begin
          123.456f0 |> deg2rad,
     )
 
-    OrbitStateVector(
+    show(IOBuffer(), ke)
+    show(IOBuffer(), MIME("text/plain"), ke)
+    show(IOBuffer(), ke_f32)
+    show(IOBuffer(), MIME("text/plain"), ke_f32)
+
+    sv = OrbitStateVector(
         date_to_jd(1986, 6, 19, 18, 35, 0),
         [-3.107e3,  1.954e6, 6.110e6],
         [ 6.337e3, -1.470e3, 3.684e3]
     )
 
-    sv = OrbitStateVector(
+    sv_f32 = OrbitStateVector(
         date_to_jd(1986, 6, 19, 18, 35, 0),
         [-3.107f3,  1.954f6, 6.110f6],
         [ 6.337f3, -1.470f3, 3.684f3]
     )
+
+    show(IOBuffer(), sv)
+    show(IOBuffer(), MIME("text/plain"), sv)
+    show(IOBuffer(), sv_f32)
+    show(IOBuffer(), MIME("text/plain"), sv_f32)
+
+    # Conversions
+    # ======================================================================================
+
+    kepler_to_rv(ke)
+    kepler_to_rv(ke_f32)
+    rv_to_kepler(sv.r, sv.v)
+    rv_to_kepler(sv.r, sv.v, 0.0)
+    rv_to_kepler(sv_f32.r, sv_f32.v)
+    rv_to_kepler(sv_f32.r, sv_f32.v, 0.0)
+
+    kepler_to_sv(ke)
+    kepler_to_sv(ke_f32)
+    sv_to_kepler(sv)
+    sv_to_kepler(sv_f32)
 
     ########################################################################################
     #                                         Time
