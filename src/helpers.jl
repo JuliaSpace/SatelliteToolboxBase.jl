1cbe0eefb8e6f6be0c36629acc4e17d33598afb8
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-06-14 09:50:08 -0300

    :wrench: Use the vector indices in get_partition

--- src/helpers.jl
+++ src/helpers.jl
@@ -35,10 +35,10 @@ end
 # == Threads ===============================================================================
 
 """
-    get_partition(cp::Integer, v::AbstractVector, np::Integer) -> Int, Int
+    get_partition(cp::Integer, inds::AbstractVector, np::Integer) -> Int, Int
 
-Return the `cp`-th partition (start and end indices) of the vector `v` considering that we
-are partitioning it into `np` parts.
+Return the `cp`-th partition (start and end indices) of a vector with indices `inds`
+considering that we are partitioning it into `np` parts.
 
 This function is useful to splitting input information for spawning multiple tasks.
 
@@ -52,8 +52,10 @@ This function is useful to splitting input information for spawning multiple tas
 - `Int`: Current partition start index.
 - `Int`: Current partition last index.
 """
-function get_partition(cp::Integer, v::AbstractVector, np::Integer)
-    num_elements = length(v)
+function get_partition(cp::Integer, inds::AbstractVector, np::Integer)
+    num_elements = length(inds)
+
+    num_elements == 0 && return 0, 0
 
     # Check inputs.
     np = min(np, num_elements)
@@ -61,7 +63,7 @@ function get_partition(cp::Integer, v::AbstractVector, np::Integer)
 
     len, rem = divrem(num_elements, np)
 
-    i₀ = firstindex(v) + (cp - 1) * len
+    i₀ = first(inds) + (cp - 1) * len
     i₁ = i₀ + len - 1
 
     i₀ += cp <= rem ? cp - 1 : rem
--- test/helpers.jl
+++ test/helpers.jl
@@ -5,26 +5,28 @@
 ############################################################################################
 
 @testset "Helpers" begin
-    i₀, i₁ = SatelliteToolboxBase.get_partition(1, 1:1:10, 3)
+    vt = 1:2:20
+    inds = eachindex(vt)
+    i₀, i₁ = SatelliteToolboxBase.get_partition(1, vt, 3)
     @test i₀ == 1
     @test i₁ == 4
 
-    i₀, i₁ = SatelliteToolboxBase.get_partition(2, 1:1:10, 3)
+    i₀, i₁ = SatelliteToolboxBase.get_partition(2, vt, 3)
     @test i₀ == 5
     @test i₁ == 7
 
-    i₀, i₁ = SatelliteToolboxBase.get_partition(3, 1:1:10, 3)
+    i₀, i₁ = SatelliteToolboxBase.get_partition(3, vt, 3)
     @test i₀ == 8
     @test i₁ == 10
 
     for i in 1:10
-        i₀, i₁ = SatelliteToolboxBase.get_partition(i, 1:1:10, 100)
+        i₀, i₁ = SatelliteToolboxBase.get_partition(i, vt, 100)
         @test i₀ == i
         @test i₁ == i
     end
 
     for i in 11:100
-        i₀, i₁ = SatelliteToolboxBase.get_partition(i, 1:1:10, 100)
+        i₀, i₁ = SatelliteToolboxBase.get_partition(i, vt, 100)
         @test i₀ == 10
         @test i₁ == 10
     end
